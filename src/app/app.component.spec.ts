c09ecab579416cdc6df778e31fc7d8a7b25ce733
erbynn <john.erbynn@gmail.com>
2020-12-17 06:24:59 +0000

    refactor: clean code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-integration-testing app is running!');
+    expect(compiled.querySelector('.content h3').textContent).toContain('angular-integration-testing app is running!');
   });
 });
